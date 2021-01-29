7af5e678fb5b86b80a72e292df7a41817c16c48e
= <rankovictamaraa@gmail.com>
2021-01-29 11:49:32 +0100

    login i landing page kreirane

--- pharmacies/src/app/app.module.ts
+++ pharmacies/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pharmacies/src/app/system-admin/system-admin-routing.module.ts
+++ pharmacies/src/app/system-admin/system-admin-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from '../shared/page-not-found/page-not-found.component';
+import { LandingPageComponent } from '../unauthenticated-user/landing-page/landing-page.component';
 
 const routes: Routes = [
+  {path: '', component: LandingPageComponent},
   {path:'**', component: PageNotFoundComponent}
 ];
 
