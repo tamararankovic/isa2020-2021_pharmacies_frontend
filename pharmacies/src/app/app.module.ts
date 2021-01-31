f217e3afc6aefb6bfae02e3ccb7fd4102bd066a7
= <rankovictamaraa@gmail.com>
2021-01-31 22:21:13 +0100

    omogucen rad sa mapama

--- pharmacies/src/app/app.module.ts
+++ pharmacies/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon'
 import { HttpClientModule } from '@angular/common/http';
+
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
     AppComponent
@@ -20,7 +22,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatToolbarModule,
     MatButtonModule,
     HttpClientModule,
-    MatIconModule
+    MatIconModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyArX13dkqG1oKxB4wnrinrHr7pzuNrR2wQ',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
