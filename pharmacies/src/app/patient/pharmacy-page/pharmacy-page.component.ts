abdd1678948013e5cc0073a7e4f8b42eed2580d4
= <rankovictamaraa@gmail.com>
2021-01-31 22:22:27 +0100

    dizajn profila apoteke koji se prikazuje pacijentu

---
+++ pharmacies/src/app/patient/pharmacy-page/pharmacy-page.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PharmacyInfoDto } from '../DTOs/pharmacy-info-dto';
+import { PharmacyProfileExaminationDto } from '../DTOs/pharmacy-profile-examination-dto';
+import { PharmacyService } from '../service/pharmacy.service';
+
+@Component({
+  selector: 'app-pharmacy-page',
+  templateUrl: './pharmacy-page.component.html',
+  styleUrls: ['./pharmacy-page.component.css']
+})
+export class PharmacyPageComponent implements OnInit {
+
+  public data : PharmacyInfoDto = new PharmacyInfoDto("", "", "", 0, [], [], [], []);
+
+  public displayedColumnsMedicine = ["Name", "Reserve"];
+  public displayedColumnsPharmacist = ["Name"];
+  public displayedColumnsDermatologist = ["Name"];
+  public displayedColumnsExamination = ["Date", "StartTime", "Duration", "Dermatologist", "Price", "Schedule"];
+
+  public showMedicines = false;
+  public showPharmacists = false;
+  public showDermatologists = false;
+  public showExaminations = false;
+
+  public latitude: number = 0;
+  public longitude: number = 0;
+  public zoom:number = 15;
+
+  private geocoder;
+
+  constructor(private pharmacyService : PharmacyService, private route : ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.pharmacyService.getPharmacyInfo(parseInt(this.route.snapshot.paramMap.get('id'))).subscribe(
+      (val) => {this.data = val;
+                for (let e of this.data.examinations) {
+                  e.startDateTime = new Date(e.startDateTime[0], e.startDateTime[1]-1, e.startDateTime[2], e.startDateTime[3], e.startDateTime[4]);
+                }
+                this.geocoder = new google.maps.Geocoder();
+                this.geocoder.geocode({ address: this.data.address }, (results, status) => {
+                  if (status === "OK") {
+                    this.latitude = +results[0].geometry.location.lat();
+                    this.longitude = +results[0].geometry.location.lng();
+                    this.zoom = 15;
+                  } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                  }
+                });
+              }
+    )
+  }
+
+  toggleMedicineList() {
+    this.showMedicines = !this.showMedicines;
+    this.showPharmacists = false;
+    this.showDermatologists = false;
+    this.showExaminations = false;
+  }
+
+  togglePharmacistList() {
+    this.showPharmacists = !this.showPharmacists;
+    this.showMedicines = false;
+    this.showDermatologists = false;
+    this.showExaminations = false;
+  }
+
+  toggleDermatologistList() {
+    this.showDermatologists = !this.showDermatologists;
+    this.showPharmacists = false;
+    this.showMedicines = false;
+    this.showExaminations = false;
+  }
+
+  toggleExaminationList() {
+    this.showExaminations = !this.showExaminations;
+    this.showPharmacists = false;
+    this.showDermatologists = false;
+    this.showMedicines = false;
+  }
+}
