94412951f24e7dbcb28be0fbef9688960bbe9d86
anja-meseldzic <aanja.meseldzic@gmail.com>
2021-02-01 13:56:42 +0100

    patient profile form created

--- pharmacies/src/app/patient/patient.module.ts
+++ pharmacies/src/app/patient/patient.module.ts
@@ -4,13 +4,29 @@ import { CommonModule } from '@angular/common';
 import { PatientRoutingModule } from './patient-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+ 
 
 
 @NgModule({
   declarations: [HomePageComponent, ProfileComponent],
   imports: [
     CommonModule,
-    PatientRoutingModule
+    PatientRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSnackBarModule
+
   ]
 })
 export class PatientModule { }
--- pharmacies/src/app/patient/profile/profile.component.ts
+++ pharmacies/src/app/patient/profile/profile.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  public name: string = "Anja";
   constructor() { }
 
   ngOnInit(): void {
